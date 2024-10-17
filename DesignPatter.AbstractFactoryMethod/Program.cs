570f90b279ea61b32b383940b2d8aaefd660c321
Eddie <edrismoavenghafouri@gmail.com>
2024-10-17 12:05:06 +0330

    adding main method

---
+++ DesignPatter.AbstractFactoryMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿
+IPaymentGatewayFactory stripeFactory = new StripePaymentGatewayFactory();
+PaymentService stripeService = new PaymentService(stripeFactory);
+stripeService.MakePayment(100.00m);
+stripeService.RefundPayment(25.00m);
+
+Console.WriteLine();
+
+IPaymentGatewayFactory payPalFactory = new PayPalPaymentGatewayFactory();
+PaymentService payPalService = new PaymentService(payPalFactory);
+payPalService.MakePayment(150.00m);
+payPalService.RefundPayment(50.00m);
+
+Console.WriteLine();
+
+IPaymentGatewayFactory squareFactory = new DigiPayPaymentGatewayFactory();
+PaymentService squareService = new PaymentService(squareFactory);
+squareService.MakePayment(200.00m);
+squareService.RefundPayment(100.00m);
