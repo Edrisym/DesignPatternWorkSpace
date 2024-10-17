40cf1f6f1cc8417867ffbece3d745804c7bd98cc
Eddie <edrismoavenghafouri@gmail.com>
2024-10-17 12:04:31 +0330

    Adding concrete products

---
+++ DesignPatter.AbstractFactoryMethod/ConcreteProducts/PayPalPaymentProcessor.cs
@@ -0,0 +1,9 @@
+namespace DesignPatter.AbstractFactoryMethod.ConcreteProducts;
+
+public class PayPalPaymentProcessor : IPaymentProcessor
+{
+    public void ProcessPayment(decimal amount)
+    {
+        Console.WriteLine($"Processing ${amount} payment via PayPal.");
+    }
+}
---
+++ DesignPatter.AbstractFactoryMethod/ConcreteProducts/StripeRefundProcessor.cs
@@ -0,0 +1,9 @@
+namespace DesignPatter.AbstractFactoryMethod.ConcreteProducts;
+
+public class StripeRefundProcessor : IRefundProcessor
+{
+    public void ProcessRefund(decimal amount)
+    {
+        Console.WriteLine($"Processing ${amount} refund via Stripe.");
+    }
+}
